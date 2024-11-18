57addd59f08b3361fbda266ef740ef31d779ff68
Kamrul Hassan <hassans.kamrul@gmail.com>
2024-11-19 01:31:14 +0600

    donation functionality for each card has been done partially

---
+++ js/donate.js
@@ -0,0 +1,79 @@
+// Noakhali button
+document
+  .getElementById("noakhali_donate_button")
+  .addEventListener("click", function () {
+    const inputValue = donateInputBalance("noakhali-input-field");
+    const mainBalance = donateCurrentBalanceAmount("current-main-balance");
+    if (inputValue > 0 && mainBalance >= inputValue) {
+      // show a succes modal notification
+
+      document.getElementById("my_modal").showModal();
+
+      // update current Value after input
+
+      const currentBalance = donateCurrentBalanceAmount(
+        "current-Noakhali-balance"
+      );
+      // update the donation amount (eg: noakhali)
+      updateDonationValue(
+        "current-Noakhali-balance",
+        inputValue,
+        currentBalance
+      );
+
+      // Decrease the amount from main balance
+      UpdateMainBalance("current-main-balance", mainBalance, inputValue);
+    } else {
+      alert("invalid donation amount!!");
+    }
+  });
+
+// Feni Button
+document
+  .getElementById("feni_donate_button")
+  .addEventListener("click", function () {
+    const inputValue = donateInputBalance("feni-input-field");
+    const mainBalance = donateCurrentBalanceAmount("current-main-balance");
+    if (inputValue > 0 && mainBalance >= inputValue) {
+      // show a succes modal notification
+
+      document.getElementById("my_modal").showModal();
+
+      // update current Value after input
+
+      const currentBalance = donateCurrentBalanceAmount("current-feni-balance");
+      // update the donation amount (eg: noakhali)
+      updateDonationValue("current-feni-balance", inputValue, currentBalance);
+
+      // Decrease the amount from main balance
+      UpdateMainBalance("current-main-balance", mainBalance, inputValue);
+    } else {
+      alert("invalid donation amount!!");
+    }
+  });
+
+//  quota button
+document
+  .getElementById("quota_donate_button")
+  .addEventListener("click", function () {
+    const inputValue = donateInputBalance("quota-input-field");
+    const mainBalance = donateCurrentBalanceAmount("current-main-balance");
+    if (inputValue > 0 && mainBalance >= inputValue) {
+      // show a succes modal notification
+
+      document.getElementById("my_modal").showModal();
+
+      // update current Value after input
+
+      const currentBalance = donateCurrentBalanceAmount(
+        "current-quota-balance"
+      );
+      // update the donation amount (eg: noakhali)
+      updateDonationValue("current-quota-balance", inputValue, currentBalance);
+
+      // Decrease the amount from main balance
+      UpdateMainBalance("current-main-balance", mainBalance, inputValue);
+    } else {
+      alert("invalid donation amount!!");
+    }
+  });
---
+++ js/main.js
@@ -0,0 +1,21 @@
+function donateInputBalance(id) {
+  const inputValue = document.getElementById(id).value;
+  const inputValueConvert = parseFloat(inputValue) || 0; //take 0 if parsefloat can't convert
+  return inputValueConvert;
+}
+
+function donateCurrentBalanceAmount(id) {
+  const currentBalance = document.getElementById(id).innerText;
+  const CurrentBalanceConvert = parseFloat(currentBalance) || 0; //take 0 if parsefloat can't convert
+  return CurrentBalanceConvert;
+}
+
+function updateDonationValue(id, inputValue, currentBalance) {
+  const updateBalance = inputValue + currentBalance;
+  document.getElementById(id).innerText = updateBalance;
+  return updateBalance;
+}
+function UpdateMainBalance(id, mainBalance, inputValue) {
+  const UpdateMainBalance = mainBalance - inputValue;
+  document.getElementById(id).innerText = UpdateMainBalance;
+}
